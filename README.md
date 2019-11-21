b7e52e134427cc936c50eec32d3fd02981e66040
CregskiN <272473132@qq.com>
2019-11-21 13:17:32 +0800

    :sparkles: add babel

---
+++ README.md
@@ -0,0 +1,13 @@
+## 本地构建
+
+### 使用模块
+```
+npm install --save-dev gulp # 安装gulp模块
+npm install gulp-less # 安装gulp-less模块
+npm install del # 安装del模块
+npm install --save-dev gulp-autoprefixer # 自动加前缀
+npm install gulp-clean-css --save-dev # 压缩css
+```
+
+### 存在的问题
+> 1、gulpfile.js中function watch无法监听change事件
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,43 @@
-const gulp = require('gulp');
+const gulp = require('gulp'); // src dest series
 const less = require('gulp-less');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+
+async function delPreBuild() {
+    await del.sync('build');
+}
 
 
 function lessTask() {
-    return gulp.src('src/**/*.less')
-            .pipe(less())
-            .pipe(gulp.dest('build'));
-
-}
-
-function defaultTask() {
-    return gulp.src('src/**/*.less')
+    return gulp.src('src/**/*')
         .pipe(less())
-        .pipe(gulp.dest('build2'));
-
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'Firefox > 20'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build'));
 }
 
-exports.default = gulp.series(lessTask, defaultTask);
+
+async function finishTask() {
+    await console.log('done!');
+}
+
+
+let taskSeries = gulp.series(delPreBuild, lessTask, finishTask);
+
+function watch () {
+    const watcher = gulp.watch('src/**/*', taskSeries);
+    watcher.on('change', event => {
+        console.log(event);
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+}
+
+taskSeries = gulp.parallel(watch, taskSeries);
+
+exports.default = taskSeries;
+
